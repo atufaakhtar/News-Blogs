2c71826c250c0e7e87409231be68a081ecefe8f8
atufaakhtar <atufaakhtar123@gmail.com>
2025-02-06 01:19:31 +0530

    minor update

--- news-blogs-app/src/Components/Blogs.jsx
+++ news-blogs-app/src/Components/Blogs.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import userImg from '../assets/images/user.jpg';
 import './Blogs.css';
 
 const Blogs = ({onBack}) => {
+
+    const [showForm, setShowForm] = useState(false);
+
     return (
         <div className="blogs">
             <div className="blogs-left">
                 <img src={userImg} alt="User Image" />
             </div>
             <div className="blogs-right">
-                {/*<button className='post-btn'>Create New Post</button>*/}
-                <div className="blogs-right-form">
+                {showForm ? (<div className="blogs-right-form">
                     <h1>New Post</h1>
                     <form>
                         <div className="img-upload">
@@ -23,7 +26,10 @@ const Blogs = ({onBack}) => {
                         <textarea className="text-input" placeholder="Add Text"></textarea>
                         <button type='submit' className='submit-btn'>Submit Button</button>
                     </form>
-                </div>
+                </div>) :
+                (<button className='post-btn' onClick={()=> setShowForm(true)}>Create New Post</button>)
+                }
+                
                 <button className="blogs-close-btn" onClick={onBack}>
                     Back <i className='bx bx-chevron-right'></i>
                 </button>
