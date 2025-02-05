b9be6ae47f8dcc7d08fa085556e2fd97898203e6
atufaakhtar <atufaakhtar123@gmail.com>
2025-02-05 17:41:37 +0530

    weather widgit structure

--- news-blogs-app/src/Components/Weather.jsx
+++ news-blogs-app/src/Components/Weather.jsx
@@ -2,7 +2,23 @@ import './Weather.css';
 
 const Weather = () => {
     return (
-        <div className='weather'>Weather</div>
+        <div className='weather'>
+            <div className="search">
+                <div className="search-top">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <div className="location">Greater Noida</div>
+                </div>
+                <div className="search-location">
+                    <input type="text" placeholder='Enter location' />
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+            <div className="weather-data">
+                <i className='bx bxs-sun'></i>
+                <div className="weather-type">Clear</div>
+                <div className="temp">28°</div>
+            </div>
+        </div>
     )
 }
 
